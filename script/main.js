8ba586b089ea57ae848e96a9bd59d63d0cf0dc1b
Septian Adi Pratama <septian.adipratama85@outlook.com>
2021-01-23 00:47:43 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -195,8 +195,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
+      x: -120,
+      y: 400,
       rotation: -180,
       opacity: 0
     })
